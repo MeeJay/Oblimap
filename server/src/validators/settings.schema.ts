7c90b56eee9ebdbd031dd0829a1e70109b49c013
MeeJay <meejayproduction@gmail.com>
2026-03-29 17:31:32 +0200

    Regular update [dev]

--- server/src/validators/settings.schema.ts
+++ server/src/validators/settings.schema.ts
@@ -3,16 +3,23 @@ import { SETTINGS_KEYS } from '@oblimap/shared';
 
 const settingsKeyValues = Object.values(SETTINGS_KEYS) as [string, ...string[]];
 
+const settingValue = z.union([
+  z.number(),
+  z.boolean(),
+  z.string(),
+  z.array(z.union([z.string(), z.number()])),
+]);
+
 export const setSettingSchema = z.object({
   key: z.enum(settingsKeyValues),
-  value: z.number(),
+  value: settingValue,
 });
 
 export const setSettingsBulkSchema = z.object({
   overrides: z.array(
     z.object({
       key: z.enum(settingsKeyValues),
-      value: z.number(),
+      value: settingValue,
     }),
   ),
 });
